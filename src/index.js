61057bb3fc3f9488502f105bc3473d40ac323c98
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2025-01-17 19:10:21 +0200

    customizing all components with their different properties

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div className="container">
+    <div className="container ">
       <Header />
       <Menu />
       <Footer />
@@ -70,11 +70,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 5}</span>
+      </div>
+    </div>
   );
 }
 function Footer() {
@@ -93,16 +116,6 @@ function Footer() {
   );
 
   //return React.createElement("footer", null, "We're currently open!");
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 // React v18
