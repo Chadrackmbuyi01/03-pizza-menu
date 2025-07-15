bf6ff465818129efb39a16c3a6e5d20503d9ae1e
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2025-07-15 02:46:42 +0200

    added a proper title and implemented ternary operator to differentiate in and out-stock items from the meny component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const pizzaData = [
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
     photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
+    soldOut: true, // This pizza is sold out
   },
   {
     name: "Pizza Margherita",
@@ -122,7 +122,7 @@ function Pizza({ pizzaObj }) {
   //if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
